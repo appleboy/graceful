9cef3f3c7dc59014fff40b4d15431cc4bad45f98
Bo-Yi Wu <appleboy.tw@gmail.com>
2022-01-02 18:02:34 +0800

    chore(job): add running job
    Signed-off-by: Bo-Yi Wu <appleboy.tw@gmail.com>

--- manager.go
+++ manager.go
@@ -13,23 +13,40 @@ var manager *Manager
 
 var initOnce = sync.Once{}
 
+type RunningJob func(context.Context) error
+
 // Manager manages the graceful shutdown process
 type Manager struct {
-	lock             *sync.RWMutex
-	doneCtx          context.Context
-	doneCtxCancel    context.CancelFunc
-	logger           Logger
-	runningWaitGroup sync.WaitGroup
-	toRunTask        []func(context.Context) error
+	lock              *sync.RWMutex
+	shutdownCtx       context.Context
+	shutdownCtxCancel context.CancelFunc
+	doneCtx           context.Context
+	doneCtxCancel     context.CancelFunc
+	logger            Logger
+	runningWaitGroup  sync.WaitGroup
+	errors            []error
 }
 
 func (g *Manager) start(ctx context.Context) {
+	g.shutdownCtx, g.shutdownCtxCancel = context.WithCancel(ctx)
 	g.doneCtx, g.doneCtxCancel = context.WithCancel(ctx)
 
 	go g.handleSignals(ctx)
 }
 
+// DoGracefulShutdown graceful shutdown all task
 func (g *Manager) DoGracefulShutdown() {
+	g.shutdownCtxCancel()
+	go func() {
+		g.waitForJobs()
+		g.lock.Lock()
+		g.doneCtxCancel()
+		g.lock.Unlock()
+	}()
+}
+
+func (g *Manager) waitForJobs() {
+	g.runningWaitGroup.Wait()
 }
 
 func (g *Manager) handleSignals(ctx context.Context) {
@@ -63,6 +80,31 @@ func (g *Manager) handleSignals(ctx context.Context) {
 	}
 }
 
+func (g *Manager) AddRunningJob(f RunningJob) {
+	g.runningWaitGroup.Add(1)
+
+	go func() {
+		// to handle panic cases from inside the worker
+		// in such case, we start a new goroutine
+		defer func() {
+			g.runningWaitGroup.Done()
+			if err := recover(); err != nil {
+				g.logger.Error(err)
+			}
+		}()
+		if err := f(g.shutdownCtx); err != nil {
+			g.lock.Lock()
+			g.errors = append(g.errors, err)
+			g.lock.Unlock()
+		}
+	}()
+}
+
+// Done allows the manager to be viewed as a context.Context.
+func (g *Manager) Done() <-chan struct{} {
+	return g.doneCtx.Done()
+}
+
 // NewManager initial the Manager
 func NewManager(opts ...Option) *Manager {
 	o := newOptions(opts...)
@@ -70,6 +112,7 @@ func NewManager(opts ...Option) *Manager {
 		manager = &Manager{
 			lock:   &sync.RWMutex{},
 			logger: o.logger,
+			errors: make([]error, 0),
 		}
 	})
 
