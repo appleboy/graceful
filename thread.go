6b77f3c393ac7fa47b884ff1de47f7f11f1a5f67
Bo-Yi Wu <appleboy.tw@gmail.com>
2023-11-26 11:23:16 +0800

    feat: add new methods to routineGroup struct and test case
    - Add a new method `Run` to the `routineGroup` struct in `thread.go`
    - Add a new method `Wait` to the `routineGroup` struct in `thread.go`
    - Add a new test case for the `Run` method in `thread_test.go`
    
    Signed-off-by: Bo-Yi Wu <appleboy.tw@gmail.com>

--- thread.go
+++ thread.go
@@ -2,14 +2,17 @@ package graceful
 
 import "sync"
 
+// routineGroup represents a group of goroutines.
 type routineGroup struct {
 	waitGroup sync.WaitGroup
 }
 
+// newRoutineGroup creates a new routineGroup.
 func newRoutineGroup() *routineGroup {
 	return new(routineGroup)
 }
 
+// Run runs a function in a new goroutine.
 func (g *routineGroup) Run(fn func()) {
 	g.waitGroup.Add(1)
 
@@ -19,6 +22,7 @@ func (g *routineGroup) Run(fn func()) {
 	}()
 }
 
+// Wait waits for all goroutines to finish.
 func (g *routineGroup) Wait() {
 	g.waitGroup.Wait()
 }
--- thread_test.go
+++ thread_test.go
@@ -1,1 +1,28 @@
 package graceful
+
+import (
+	"testing"
+)
+
+func TestRun(t *testing.T) {
+	g := newRoutineGroup()
+
+	// Define a flag to check if the function was executed
+	var executed bool
+
+	// Define the function to be executed
+	fn := func() {
+		executed = true
+	}
+
+	// Run the function in a goroutine
+	g.Run(fn)
+
+	// Wait for the goroutine to finish
+	g.Wait()
+
+	// Check if the function was executed
+	if !executed {
+		t.Error("Function was not executed")
+	}
+}
